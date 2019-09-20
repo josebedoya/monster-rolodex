ba13e4419319cc280c1a1db70651ab6ffd930fdd
Jose Bedoya <jossephb@gmail.com>
2019-09-20 18:33:36 -0500

    Updates

---
+++ precache-manifest.835472e6d04268836b160d471316f7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0156f47b7d58749e762a554e5cdc42",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e070e93ad01a2cbdb617",
+    "url": "/monster-rolodex/static/css/main.76482222.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "e070e93ad01a2cbdb617",
+    "url": "/monster-rolodex/static/js/main.b48a7c9f.chunk.js"
+  },
+  {
+    "revision": "52b3135f1aa77ef4ddba",
+    "url": "/monster-rolodex/static/js/runtime~main.b7028ca9.js"
+  }
+]);
