8c91351a3085b5af0387cffcd84a4dc968618890
Jose Bedoya <jossephb@gmail.com>
2019-09-20 18:20:00 -0500

    adding files for github pages

---
+++ src/components/card/card.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./card.style.css";
+
+export const Card = props => {
+  const { id, name, email } = props.monster;
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
